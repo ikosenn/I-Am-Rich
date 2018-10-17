a9653462a4490248f6dbc10f56f0491b2f161b63
ikosenn <i@ikosenn.me>
2018-10-16 23:51:42 -0700

    create ios app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ian Kosen on 08/10/2018.
+//  Copyright © 2018 Ian Kosen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
